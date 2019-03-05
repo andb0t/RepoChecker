c8419dfbfb12930bd228fe79172accc984c09f64
Andreas Alexander Maier <andb0t@pm.me>
2019-03-05 11:41:54 +0100

    Make ordered dict

--- repo_checker.py
+++ repo_checker.py
@@ -1,5 +1,6 @@
 """This script checks github repositories for actvity."""
 import json
+from collections import OrderedDict
 from datetime import datetime, timedelta
 import urllib.request
 
@@ -10,6 +11,7 @@ urls = [
     "https://api.github.com/repos/andb0t/MLlab",
     "https://api.github.com/repos/smicallef/spiderfoot",
     "https://api.github.com/repos/nicolasbeauvais/laravel-botscout"
+    # "https://gitlab.com/api/v4/projects/gitlab-org%2Fci-cd%2Fdocker-machine"
 ]
 
 responses = []
@@ -18,19 +20,25 @@ for url in urls:
     data = response.read()
     responses.append(json.loads(data))
 
-headers = [
-    "updated_at",
-    "watchers",
-    "subscribers_count",
-    "open_issues",
-    "forks"
-]
+headers = OrderedDict({
+    "updated": {"github": "updated_at", "gitlab": "updated_at"},
+    "watchers": {"github": "watchers", "gitlab": "watchers"},
+    "subscribers": {"github": "subscribers_count", "gitlab": "subscribers_count"},
+    "open_issues": {"github": "open_issues", "gitlab": "open_issues"},
+    "forks": {"github": "forks", "gitlab": "forks"}
+})
 
 table = []
 for url, response in zip(urls, responses):
     entry = [url]
-    for header in headers:
-        text = response[header]
+    for header in headers.values():
+        if 'github' in url:
+            key = header['github']
+        elif 'gitlab' in url:
+            key = header['gitlab']
+        else:
+            raise NotImplementedError('unknown git hosting service')
+        text = response[key]
         try:
             time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ')
             # text = time.strftime('%d.%m.%Y')
@@ -44,4 +52,4 @@ for url, response in zip(urls, responses):
 table = sorted(table, key=lambda x: x[0], reverse=True)
 
 # print(responses[0].keys())
-print(tabulate.tabulate(table, ['URL', *headers], tablefmt="grid"))
+print(tabulate.tabulate(table, ['URL', *list(headers.keys())], tablefmt="grid"))
