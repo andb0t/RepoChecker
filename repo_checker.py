a83e7a7b57001a3310e18e7a53cd88501002d42a
Andreas Alexander Maier <andb0t@pm.me>
2019-03-05 12:33:21 +0100

    Add bitbucket url

--- repo_checker.py
+++ repo_checker.py
@@ -7,11 +7,13 @@ import urllib.request
 import tabulate
 
 
+# the API endpoints to check. For gitlab, replace repo / by %2F
 urls = [
     "https://api.github.com/repos/andb0t/MLlab",
     "https://api.github.com/repos/smicallef/spiderfoot",
     "https://api.github.com/repos/nicolasbeauvais/laravel-botscout",
-    "https://gitlab.com/api/v4/projects/gitlab-org%2Fci-cd%2Fdocker-machine"
+    "https://gitlab.com/api/v4/projects/gitlab-org%2Fci-cd%2Fdocker-machine",
+    "https://api.bitbucket.org/2.0/repositories/tutorials/tutorials.bitbucket.org"
 ]
 
 responses = []
@@ -21,11 +23,11 @@ for url in urls:
     responses.append(json.loads(data))
 
 headers = OrderedDict({
-    "updated": {"github": "updated_at", "gitlab": "last_activity_at"},
-    "watchers": {"github": "watchers", "gitlab": None},
-    "stars": {"github": "subscribers_count", "gitlab": "star_count"},
-    "forks": {"github": "forks", "gitlab": "forks_count"},
-    "open issues": {"github": "open_issues", "gitlab": "open_issues_count"},
+    "updated": {"github": "updated_at", "gitlab": "last_activity_at", "bitbucket": "updated_on"},
+    "watchers": {"github": "watchers", "gitlab": None, "bitbucket": None},
+    "stars": {"github": "subscribers_count", "gitlab": "star_count", "bitbucket": None},
+    "forks": {"github": "forks", "gitlab": "forks_count", "bitbucket": None},
+    "open issues": {"github": "open_issues", "gitlab": "open_issues_count", "bitbucket": None},
 })
 
 table = []
@@ -36,8 +38,10 @@ for url, response in zip(urls, responses):
             key = header['github']
         elif 'gitlab' in url:
             key = header['gitlab']
+        elif 'bitbucket' in url:
+            key = header['bitbucket']
         else:
-            raise NotImplementedError('unknown git hosting service')
+            raise NotImplementedError('unknown git hosting service in', url)
         try:
             text = response[key]
         except KeyError as exc:
@@ -47,7 +51,10 @@ for url, response in zip(urls, responses):
             try:
                 time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ')
             except ValueError:
-                time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%fZ')
+                try:
+                    time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%fZ')
+                except ValueError:
+                    time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%f+00:00')
             # text = time.strftime('%d.%m.%Y')
             diff_days = (datetime.now() - time).days
             text = '%s days ago' % diff_days
