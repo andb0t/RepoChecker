a62e59effa8c9e361e4b7d8eb500ff438eb93a0c
Andreas Alexander Maier <andb0t@pm.me>
2019-03-05 12:23:34 +0100

    Add gitlab repo

--- repo_checker.py
+++ repo_checker.py
@@ -10,8 +10,8 @@ import tabulate
 urls = [
     "https://api.github.com/repos/andb0t/MLlab",
     "https://api.github.com/repos/smicallef/spiderfoot",
-    "https://api.github.com/repos/nicolasbeauvais/laravel-botscout"
-    # "https://gitlab.com/api/v4/projects/gitlab-org%2Fci-cd%2Fdocker-machine"
+    "https://api.github.com/repos/nicolasbeauvais/laravel-botscout",
+    "https://gitlab.com/api/v4/projects/gitlab-org%2Fci-cd%2Fdocker-machine"
 ]
 
 responses = []
@@ -21,35 +21,40 @@ for url in urls:
     responses.append(json.loads(data))
 
 headers = OrderedDict({
-    "updated": {"github": "updated_at", "gitlab": "updated_at"},
-    "watchers": {"github": "watchers", "gitlab": "watchers"},
-    "subscribers": {"github": "subscribers_count", "gitlab": "subscribers_count"},
-    "open_issues": {"github": "open_issues", "gitlab": "open_issues"},
-    "forks": {"github": "forks", "gitlab": "forks"}
+    "updated": {"github": "updated_at", "gitlab": "last_activity_at"},
+    "watchers": {"github": "watchers", "gitlab": None},
+    "stars": {"github": "subscribers_count", "gitlab": "star_count"},
+    "forks": {"github": "forks", "gitlab": "forks_count"},
+    "open issues": {"github": "open_issues", "gitlab": "open_issues_count"},
 })
 
 table = []
 for url, response in zip(urls, responses):
     entry = [url]
-    for header in headers.values():
+    for col, header in headers.items():
         if 'github' in url:
             key = header['github']
         elif 'gitlab' in url:
             key = header['gitlab']
         else:
             raise NotImplementedError('unknown git hosting service')
-        text = response[key]
         try:
-            time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ')
+            text = response[key]
+        except KeyError as exc:
+            print('Key not found for url {}: {}'.format(url, exc))
+            text = None
+        if col == 'updated':
+            try:
+                time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ')
+            except ValueError:
+                time = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%fZ')
             # text = time.strftime('%d.%m.%Y')
             diff_days = (datetime.now() - time).days
             text = '%s days ago' % diff_days
-        except TypeError:
-            pass
         entry.append(text)
     table.append(entry)
 
-table = sorted(table, key=lambda x: x[0], reverse=True)
+table = sorted(table, key=lambda x: int(x[1].split()[0]))
 
 # print(responses[0].keys())
 print(tabulate.tabulate(table, ['URL', *list(headers.keys())], tablefmt="grid"))
